3d24ccfb3f823de0edd1a33f14f434f647a6fa66
Aniket Gupta <gupta532@purdue.edu>
2020-04-18 18:51:12 +0530

    Reduce fraction to lowest terms

---
+++ src/EasyMath.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class EasyMath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] math = line.split("/");
+        long numerator = Long.parseLong(math[0]);
+        long denominator = Long.parseLong(math[1]);
+
+        for (int i = 2; i <= Math.max(numerator, denominator); i++) {
+            if (numerator % i == 0 && denominator % i == 0) {
+                numerator /= i;
+                denominator /= i;
+            }
+        }
+        System.out.println(numerator + "/"+  denominator);
+    }
+}
