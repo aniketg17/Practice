89b53060d9a6425f6a8e87a8bdd20003c7cf3007
Aniket Gupta <gupta532@purdue.edu>
2020-04-29 17:02:14 +0530

    HeightLineUp : Problem completed with O(n) time complexity instead of O(n^2) like before InsertionSort: Attempted bubble and exchange sort as well

--- src/HeightLineUp.java
+++ src/HeightLineUp.java
@@ -1,40 +1,47 @@
 import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class HeightLineUp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numPpl = sc.nextInt();
         sc.nextLine();
-        int[] answers = new int[numPpl];
         int[] people = new int[numPpl];
         for (int i = 0; i < numPpl; i++) {
             people[i] = sc.nextInt();
             sc.nextLine();
         }
-
-        int start = 0;
-        int end;
-
-        answers[0] = 0;
-
-        for (end = 1; end < numPpl; end++) {
-            if (people[end] <= people[start]) {
-                answers[end] = 0;
-                start++;
-            } else {
-                int count = 0;
-                for (; start >= 0 && people[start] < people[end]; start--) {
-                    count += 1;
-                }
-                answers[end] = count;
-                start = end;
-            }
-        }
-
-        for (int x : answers) {
-            System.out.println(x);
+        int[] ans = findLineup(people);
+        for (int an : ans) {
+            System.out.println(an);
         }
     }
+    
+    public static int[] findLineup(int[] people) {
+        int[] answers = new int[people.length];
+        Stack<Integer> heights = new Stack<>();
+
+        int offset = 0;
+
+        for (int i = 0; i < people.length; i++) {
+            boolean found = false;
+            while (!heights.isEmpty() && !found) {
+                if (people[heights.peek()] >= people[i]) {
+                    found = true;
+                } else {
+                    heights.pop();
+                }
+            }
+            if (!heights.isEmpty()) {
+               offset = heights.peek() + 1;
+            } else {
+                offset = 0;
+            }
+            answers[i] = i - offset;
+            heights.push(i);
+        }
+        return answers;
+    } 
 }
