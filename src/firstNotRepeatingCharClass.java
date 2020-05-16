52b582abe2b1e02dcc5d1dcc95b35b9604733a06
Aniket Gupta <gupta532@purdue.edu>
2020-05-16 16:46:56 +0530

    Attempted question: Find the first character which does not have a duplicate in the string. For example, in "abcdeabee" it would be 'c'

---
+++ src/firstNotRepeatingCharClass.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class firstNotRepeatingCharClass {
+
+    public static void main(String[] args) {
+        String s = "ngrhhqbhnsipkcoqjyviikvxbxyphsnjpdxkhtadltsuxbfbrkof";
+
+        System.out.println(firstNotRepeatingCharacter(s));
+    }
+
+    public static char  firstNotRepeatingCharacter(String s) {
+        int[] bits = new int[26];
+
+        Arrays.fill(bits, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            bits[(int) s.charAt(i) - 97] += 1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char idx = s.charAt(i);
+            if (bits[(int) idx - 97] == 1) {
+                return idx;
+            }
+        }
+
+        return '_';
+    }
+}
