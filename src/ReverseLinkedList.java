2018b5c84be49289d016281ec65e47c0aa8907f3
aniketg17 <aniketkg@gmail.com>
2020-05-21 17:41:51 +0530

    Attempted reversing a linked list

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+public class ReverseLinkedList {
+    private static class Node {
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next = head.next;
+
+        while (next != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+
+        curr.next = prev;
+
+        return curr;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(7);
+
+        head = reverseList(head);
+
+        while (head != null) {
+            System.out.print(head.val);
+            head = head.next;
+        }
+    }
+}
