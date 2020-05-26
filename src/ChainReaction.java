5ff26cd5c10ea7598bd06f715e2d43c758e4afa7
aniketg17 <aniketkg@gmail.com>
2020-05-26 11:15:34 +0530

    Added problem description

--- src/ChainReaction.java
+++ src/ChainReaction.java
@@ -1,6 +1,28 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+Chain Reaction is a game of choice. It takes in N integers, these integers are the location of ballons on the number line.
+The aim of the game is to choose the best start point that results in the maximum number of balloon bursts. The rules are as follows:
+
+You can only choose one balloon to hit. This hit will start the chain reaction.
+After the initial hit, the balloons that are at a distance of 1 from that first ballon will burst,
+this means if you hit the balloon at location 7, balloons at 6 and 8 (if they are in the there) will burst.
+The balloons that burst at time 2 will cause the balloons that are within a distance of 2 from them to burst.
+After this, the balloons that burst at time 3 will cause the balloons within a distance of 3 from them to burst and so on.
+In general terms, at time t a set of balloons will burst, each of these bursts will cause the balloons at a distance
+of t from them to burst at time t+1 and affect balloons that are within a distance of t+1, and so on.
+
+Input Format
+The first line of input contains N. The number of balloons. The remaining N lines all contain integers. These integers are the location of the balloons.
+
+Constraints
+(1≤N≤100) The locations are in the range [0,1000000000]
+
+Output Format
+Output the max number of balloons that will burst when the first hit is optimal.
+ */
+
 public class ChainReaction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
