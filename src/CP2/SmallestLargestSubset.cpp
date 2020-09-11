e0d975af0a7582ef404394b62d11c24e81c03e9b
Aniket Kumar Gupta <54602672+aniketg17@users.noreply.github.com>
2020-09-11 18:16:33 +0530

    Fixed bug with code related to bool

--- src/CP2/SmallestLargestSubset.cpp
+++ src/CP2/SmallestLargestSubset.cpp
@@ -8,47 +8,46 @@
 
 using namespace std;
 
-vector<vector<int>> combinations;
+vector<int> ret;
 vector<int> result;
 int curr_sum;
 int target;
-bool done;
+int min_sz = 1000000;
 
 void runner(vector<int> &vals, int start) {
     if (start == vals.size() || curr_sum >= target) {
-        if (curr_sum == target && combinations.empty()) {
-            combinations.push_back(result);
-            done = true;
+        if (curr_sum == target && result.size() < min_sz) {
+            ret = result;
+            min_sz = result.size();
         }
         return;
     }
-    // two choices. take current element or dont
-    if (!done) {
-        // use
-        result.push_back(vals[start]);
-        curr_sum += vals[start];
-        runner(vals, start + 1);
-        // dont use
-        result.pop_back();
-        curr_sum -= vals[start];
-        runner(vals, start+1);
-    }
+    result.push_back(vals[start]);
+    curr_sum += vals[start];
+    runner(vals, start + 1);
+    result.pop_back();
+    curr_sum -= vals[start];
+    runner(vals, start + 1);
 }
 
 int main() {
-    vector<int> input {1,2,6,3,17,82,23,234};// assuming no duplicates
+    vector<int> input{1,3,2,4,5,8};// assuming no duplicates
     unordered_map<int, int> idx;
-    done = false;
     int i = 0;
     for (int x : input) {
         idx[x] = i++;
     }
-    int val = 40; // local tests with 23, 26, 40
+    int val = 5; // local tests with 23, 26, 40
     target = val;
     curr_sum = 0;
     sort(input.begin(), input.end(), greater()); // sort in descending for greedy approach
     runner(input, 0);
-    vector<int> combo = combinations[0];
+    vector<int> combo;
+    for (int x : ret) {
+        combo.push_back(idx[x]);
+    }
     sort(combo.begin(), combo.end());
-    for (int x : combo) cout << idx[x] << " ";
+    for (int x : combo) {
+        cout << x << " ";
+    }
 }
