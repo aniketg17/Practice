129a90959d871591bd0c6447589b9560da6eff63
Aniket Kumar Gupta <54602672+aniketg17@users.noreply.github.com>
2020-09-24 18:59:36 +0530

    Kattis Knigs of the Forest completed

---
+++ src/CP2/KnigsOfTheForest.java
@@ -0,0 +1,208 @@
+package CP2;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class KnigsOfTheForest {
+    public static void main(String[] args) throws IOException {
+        Main.FastReader sc = new Main.FastReader();
+        int poolSize = sc.nextInt();
+        int years = sc.nextInt();
+        int yearEntry = sc.nextInt();
+        int strength = sc.nextInt();
+
+        ArrayList<Moose> list = new ArrayList<>();
+        list.add(new Moose(yearEntry, strength));
+        for (int i = 0; i < years + poolSize - 2; i++) {
+            Moose m = new Moose(sc.nextInt(), sc.nextInt());
+            list.add(m);
+        }
+        list.sort(Comparator.comparingInt((Moose m) -> m.year));
+        PriorityQueue<Moose> q = new PriorityQueue<>((m1, m2) -> Integer.compare(m2.strength, m1.strength));
+        for (int i = 0; i < poolSize; i++) {
+            q.add(list.get(i));
+        }
+
+        int yr = 0;
+        int ctr = poolSize;
+        for (int i = 0; i < years; i++) {
+            Moose m = q.poll();
+            if (m.year == yearEntry && m.strength == strength) {
+                System.out.println(2011 + yr);
+                return;
+            }
+            while (q.size() < poolSize && ctr < list.size()) {
+                q.add(list.get(ctr++));
+            }
+            ++yr;
+        }
+        System.out.println("unknown");
+    }
+
+    static class Moose {
+        int year;
+        int strength;
+
+        public Moose(int year, int strength) {
+            this.year = year;
+            this.strength = strength;
+        }
+    }
+}
+
+class Main {
+    static Comparator<int[]> comp = new Comparator<int[]>() {
+        public int compare(int[] a, int[] b) {
+            for (int i=0; i<a.length; i++) {
+                if (a[i] < b[i])       return -1;
+                else if (a[i] > b[i])  return 1;
+            }
+            return 0;
+        }
+    };
+
+    public static void main(String[] args) throws IOException {
+        // Use either of FastReader or Scanner for input.
+        FastReader in = new FastReader();
+        // Scanner in = new Scanner(System.in);
+
+        PrintWriter out = new PrintWriter(System.out);
+
+        out.close();
+    }
+
+    /* FastReader code from Method 4 in the post https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+       Modified nextLine() to allow arbitrary long lines,
+       Modified fillBuffer(), read() to fix some issues
+       Added next(), and hasNext()
+       Use nextInt(), nextLong(), or nextDouble() to read numbers
+       Use next() to read a string.
+       Use nextLine() to read in the next line that is not empty (i.e., it
+           contains at least one character that is > 32 (' ').
+    */
+    static class FastReader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private int MAX_LINE_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer, lineBuf;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            lineBuf = new byte[MAX_LINE_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        public FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        public boolean hasNext() throws IOException {
+            byte c;
+            while ((c = read()) != -1) {
+                if (c > ' ') {      // Find first byte bigger than ' '
+                    bufferPointer--;
+                    return true;
+                }
+            }
+            return false;
+        }
+        // return the next line that contains at least one character > ' '
+        public String nextLine() throws IOException {
+            int ctr = 0;
+            byte c;
+            boolean empty = true;
+            while ((c = read()) != -1) {
+                if (c == '\r')        continue;     // ignore '\r'
+                if (c == '\n') {
+                    if (empty)  { ctr = 0;   continue;  } // read only spaces etc. until \n
+                    else        break;
+                }
+                if (ctr == MAX_LINE_SIZE) {
+                    MAX_LINE_SIZE *= 2;
+                    lineBuf = Arrays.copyOf(lineBuf, MAX_LINE_SIZE);
+                }
+                lineBuf[ctr++] = c;
+                if (c > ' ')  empty = false;
+            }
+            return new String(lineBuf, 0, ctr);
+        }
+        public String next() throws IOException {
+            int ctr = 0;
+            byte c = read();
+            while (c <= ' ')    c = read();
+            while (c > ' ') {
+                if (ctr == MAX_LINE_SIZE) {
+                    MAX_LINE_SIZE *= 2;
+                    lineBuf = Arrays.copyOf(lineBuf, MAX_LINE_SIZE);
+                }
+                lineBuf[ctr++] = c;
+                c = read();
+            }
+            return new String(lineBuf, 0, ctr);
+        }
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')   c = read();
+            boolean neg = (c == '-');
+            if (neg)           c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)           return -ret;
+            return ret;
+        }
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')    c = read();
+            boolean neg = (c == '-');
+            if (neg)            c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)            return -ret;
+            return ret;
+        }
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)    c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+            if (neg)     return -ret;
+            return ret;
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)     fillBuffer();
+            if (bytesRead <= 0)  return -1;  // No data
+            return buffer[bufferPointer++];
+        }
+        public void close() throws IOException {
+            if (din == null)       return;
+            din.close();
+        }
+    }
+}
